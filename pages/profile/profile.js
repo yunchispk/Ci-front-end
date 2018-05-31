54a64a72902fcbae70e6bf6635fd2706ef13b467
yunchispk <yunchispk@qq.com>
2018-05-31 22:13:57 +0800

    pref:优化登录模块
    1.优化登录按钮与头像展示
    2.添加用户唯一标识与登录会话密钥

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,15 +1,75 @@
 //pages/profile/profile.js
+const app = getApp()
+
 Page({
-  getProfile(res) {
-    //console.log(res.detail.userInfo)  //console.log()函数
-    this.setData({
-      "profile": res.detail.userInfo
+  data: {
+    motto: '（只有登陆才可以查看浏览记录以及创作的作品）',
+    code: null,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
     })
   },
-  data: {
-    "profile": {
-      nickName: '请登陆'
-      //"avatarUrl": '/image/profile/avatar.png'
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        code:app.globalData.code,
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      wx.login({
+        success: res => {
+          this.setData({code : res.code})
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        }
+      })
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          code: app.globalData.code,
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.login({
+        success: res => {
+          this.setData({ code: res.code })
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        }
+      })
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            code: res.code,
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
     }
+  },
+  getUserInfo: function (e) {
+    wx.login({
+      success: res => {
+        this.setData({ code: res.code })
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
